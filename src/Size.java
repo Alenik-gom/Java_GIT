6848690c1ccbdc0181d42686f70aa540c87c1875
Alena Volchak <alena_volchak@epam.com>
2024-08-07 15:56:26 +0300

    1st releasebranch2 commit

--- src/Size.java
+++ src/Size.java
@@ -1,8 +1,10 @@
 public class Size {
     int width;
     int height;
-    public Size(int width, int height) {
+    int length;
+    public Size(int width, int height, int length) {
         this.width = width;
         this.height = height;
+        this.length = length;
     }
 }
