cfb3eb4c92425444452927f0723febcc753e43be
Alena Volchak <alena_volchak@epam.com>
2024-08-07 14:01:09 +0300

    1st hotfixes commit

--- src/Engine.java
+++ src/Engine.java
@@ -1,5 +1,10 @@
+import java.math.BigDecimal;
+
 public class Engine {
     private String name;
-    private int weight;
-    public Engine(String name, int weight) {}
+    private BigDecimal weight;
+    public Engine(String name, BigDecimal weight) {
+        this.name = name;
+        this.weight = weight;
+    }
 }
